61cbfeab2052be700a961e2b4e6065b4462b7a95
Egor <cycahosir@gmail.com>
2024-07-16 13:06:22 +0700

    Добавьте файлы проекта.

---
+++ TestMessenger/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace TestMessenger
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
